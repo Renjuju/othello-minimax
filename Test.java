3ce1988c07c48517c78b958e0d6388b1350167bf
Renju Radhakrishnan <renju467@gmail.com>
2016-11-05 13:51:38 -0400

    removed analysis method

--- Test.java
+++ Test.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 /**
@@ -7,52 +8,43 @@ import java.util.TreeSet;
  */
 public class Test {
     
-    
     public static void main(String args[]) {
         // Create the game state with the initial position for an 8x8 board:
-//        OthelloState state = new OthelloState(8);
-//        OthelloPlayer players[] = {new MiniMaxOthelloPlayer(3), new OthelloRandomPlayer()};
-//
-//        int count = 0;
-//        do{
-//            // Display the current state in the console:
-//            System.out.println("\nCurrent state, " + OthelloState.PLAYER_NAMES[state.nextPlayerToMove] + " to move:");
-//            //state.printBoard();
-//            System.out.println(state);
-//            // Get the move from the player:
-//            OthelloMove move = players[state.nextPlayerToMove].getMove(state);
-//           // System.out.println(move);
-//            state = state.applyMoveCloning(move);
-//
-//        }while(!state.gameOver());
-//
-//        // Show the result of the game:
-//        System.out.println("\nFinal state with score: " + state.score());
-//        //state.printBoard();
-//        System.out.println(state);
-        stats();
-    }
+        Scanner in = new Scanner(System.in);
+        OthelloState state = new OthelloState(8);
+        System.out.println("Which player do you want to be? 0 or 1? (invalid inputs = 1)");
+        String result = in.nextLine();
 
-    public static void stats() {
-        int wins = 0;
-        int loses = 0;
-
-        int counter = 100;
-        while(counter > 0) {
-            OthelloState state = new OthelloState(8);
-            OthelloPlayer players[] = {new MiniMaxOthelloPlayer(4), new OthelloRandomPlayer()};
-            do{
-                OthelloMove move = players[state.nextPlayerToMove].getMove(state);
-                state = state.applyMoveCloning(move);
-            } while(!state.gameOver());
-            if(state.score() > 0) {
-                wins++;
-            } else {
-                loses++;
-            }
-            counter--;
+        System.out.println("Depth of search? (more than 5 is really slow");
+        Integer depth = 2;
+        try {
+            depth = Integer.parseInt(in.nextLine());
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("Number of wins: " + wins);
-        System.out.println("Number of loses " + loses);
+
+        OthelloPlayer players[] = {new OthelloRandomPlayer(), new MiniMaxOthelloPlayer(depth)};
+        if(result.equals("0")) {
+             players[0] = new MiniMaxOthelloPlayer(depth);
+             players[1] = new OthelloRandomPlayer();
+        }
+
+        do{
+            // Display the current state in the console:
+            System.out.println("\nCurrent state, " + OthelloState.PLAYER_NAMES[state.nextPlayerToMove] + " to move:");
+            //state.printBoard();
+            System.out.println(state);
+            // Get the move from the player:
+            OthelloMove move = players[state.nextPlayerToMove].getMove(state);
+           // System.out.println(move);
+            state = state.applyMoveCloning(move);
+
+        }while(!state.gameOver());
+
+        // Show the result of the game:
+        System.out.println("\nFinal state with score: " + state.score());
+        //state.printBoard();
+        System.out.println(state);
     }
+
 }
