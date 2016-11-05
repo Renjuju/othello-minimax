2c73c64d047f3f09f37e7571cd4ae0e35b67faa2
Renju Radhakrishnan <renju@Renjus-MBP.fios-router.home>
2016-11-05 12:52:10 -0400

    minmax recursive implementation

--- MiniMaxOthelloPlayer.java
+++ MiniMaxOthelloPlayer.java
@@ -4,76 +4,73 @@ import java.util.List;
 public class MiniMaxOthelloPlayer extends OthelloPlayer {
 
 
-    public OthelloMove getMove(OthelloState state) {
-        Node node = new Node(state);
-//        Node node1 = search(node, 4);
-//
-//        System.out.println(node1.children.size());
-        System.exit(0);
-        return null;
+    private int depth;
+
+    MiniMaxOthelloPlayer(int depth) {
+        this.depth = depth;
     }
 
-    public OthelloMove nonWorkingMiniMax(OthelloState initialState) {
-        List<OthelloMove> moves = initialState.generateMoves();
-        ArrayList<OthelloState> states = new ArrayList<>();
-        ArrayList<OthelloState> minList = new ArrayList<>();
+    public OthelloMove getMove(OthelloState state) {
+        return miniMax(state);
+    }
 
+    private OthelloMove miniMax(OthelloState state) {
+        List<OthelloMove> moves = state.generateMoves();
+
+        int max = Integer.MIN_VALUE;
+        OthelloMove bestMove = null;
         for(OthelloMove move : moves) {
-            states.add(initialState.applyMoveStateCloning(move));
-        }
-
-        for(OthelloState state : states) {
-            List<OthelloMove> nextMoves = state.generateMoves();
-
-            OthelloState minState = null;
-            OthelloState tempState;
-
-            int score = Integer.MAX_VALUE;
-            for(OthelloMove nextMove : nextMoves) {
-                tempState = state.applyMoveStateCloning(nextMove);
-                if(tempState != null && tempState.score() < score) {
-                    score = tempState.score();
-                    minState = tempState;
-                    minState.setPreviousMove(nextMove);
-                }
-            }
-            minList.add(minState);
-        }
-
-        OthelloState miniMax = null;
-        int maxScore = Integer.MIN_VALUE;
-
-        if(minList.size() < 1) {
-            return null;
-        }
-
-        for(OthelloState maxState : minList) {
-            if(maxState != null && maxState.score() > maxScore) {
-                miniMax = maxState;
+            state = min(state.applyMoveCloning(move), depth);
+            if(state.score() > max) {
+                max = state.score();
+                bestMove = move;
             }
         }
+        return bestMove;
+    }
 
-        if(miniMax == null) {
-            System.exit(0);
+    private OthelloState max(OthelloState state, int depth) {
+        depth--;
+        OthelloState maxState = null;
+        List<OthelloMove> moves = state.generateMoves();
+
+        if(moves.size() == 0 || depth == 0) {
+            return state;
         }
 
-        return miniMax.getPreviousMove();
+        int val = Integer.MIN_VALUE;
+
+        OthelloState tempState;
+        for(OthelloMove move : moves) {
+            tempState = min(state.applyMoveCloning(move), depth);
+            if(tempState.score() > val) {
+                val = tempState.score();
+                maxState = tempState;
+            }
+        }
+        return maxState;
+    }
+
+    private OthelloState min(OthelloState state, int depth) {
+        depth--;
+
+        OthelloState minState = null;
+        List<OthelloMove> moves = state.generateMoves();
+
+        if(moves.size() == 0 || depth == 0) {
+            return state;
+        }
+
+        int val = Integer.MAX_VALUE;
+        OthelloState tempState;
+        for(OthelloMove move : moves) {
+            tempState = max(state.applyMoveCloning(move), depth);
+            if(tempState.score() < val) {
+                val = tempState.score();
+                minState = state;
+            }
+        }
+        return minState;
     }
 }
-//01 function minimax(node, depth, maximizingPlayer)
-//        02     if depth = 0 or node is a terminal node
-//        03         return the heuristic value of node
-//
-//        04     if maximizingPlayer
-//        05         bestValue := −∞
-//        06         for each child of node
-//        07             v := minimax(child, depth − 1, FALSE)
-//        08             bestValue := max(bestValue, v)
-//        09         return bestValue
-//
-//        10     else    (* minimizing player *)
-//        11         bestValue := +∞
-//        12         for each child of node
-//        13             v := minimax(child, depth − 1, TRUE)
-//        14             bestValue := min(bestValue, v)
-//        15         return bestValue
+
--- Test.java
+++ Test.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 /**
  *
  * @author santi
@@ -8,24 +11,48 @@ public class Test {
     public static void main(String args[]) {
         // Create the game state with the initial position for an 8x8 board:
         OthelloState state = new OthelloState(8);
-        OthelloPlayer players[] = {new OthelloRandomPlayer(),
-                                   new MiniMaxOthelloPlayer()};
+        OthelloPlayer players[] = {new MiniMaxOthelloPlayer(3), new OthelloRandomPlayer()};
 
         int count = 0;
         do{
             // Display the current state in the console:
             System.out.println("\nCurrent state, " + OthelloState.PLAYER_NAMES[state.nextPlayerToMove] + " to move:");
-            state.printBoard();
+            //state.printBoard();
+            System.out.println(state);
             // Get the move from the player:
-            OthelloMove move = players[state.nextPlayerToMove].getMove(state);            
-            System.out.println(move);
+            OthelloMove move = players[state.nextPlayerToMove].getMove(state);
+           // System.out.println(move);
             state = state.applyMoveCloning(move);
 
         }while(!state.gameOver());
 
         // Show the result of the game:
         System.out.println("\nFinal state with score: " + state.score());
-        state.printBoard();
+        //state.printBoard();
+        System.out.println(state);
+        //stats();
     }
-    
+
+    public static void stats() {
+        int wins = 0;
+        int loses = 0;
+
+        int counter = 100;
+        while(counter > 0) {
+            OthelloState state = new OthelloState(8);
+            OthelloPlayer players[] = {new MiniMaxOthelloPlayer(4), new OthelloRandomPlayer()};
+            do{
+                OthelloMove move = players[state.nextPlayerToMove].getMove(state);
+                state = state.applyMoveCloning(move);
+            } while(!state.gameOver());
+            if(state.score() > 0) {
+                wins++;
+            } else {
+                loses++;
+            }
+            counter--;
+        }
+        System.out.println("Number of wins: " + wins);
+        System.out.println("Number of loses " + loses);
+    }
 }
