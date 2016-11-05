733738c8d256be7d2bcc2856298586adc4561c2e
Renju Radhakrishnan <renju467@gmail.com>
2016-11-05 13:37:47 -0400

    modified gitignore

--- Test.java
+++ Test.java
@@ -10,27 +10,27 @@ public class Test {
     
     public static void main(String args[]) {
         // Create the game state with the initial position for an 8x8 board:
-        OthelloState state = new OthelloState(8);
-        OthelloPlayer players[] = {new MiniMaxOthelloPlayer(3), new OthelloRandomPlayer()};
-
-        int count = 0;
-        do{
-            // Display the current state in the console:
-            System.out.println("\nCurrent state, " + OthelloState.PLAYER_NAMES[state.nextPlayerToMove] + " to move:");
-            //state.printBoard();
-            System.out.println(state);
-            // Get the move from the player:
-            OthelloMove move = players[state.nextPlayerToMove].getMove(state);
-           // System.out.println(move);
-            state = state.applyMoveCloning(move);
-
-        }while(!state.gameOver());
-
-        // Show the result of the game:
-        System.out.println("\nFinal state with score: " + state.score());
-        //state.printBoard();
-        System.out.println(state);
-        //stats();
+//        OthelloState state = new OthelloState(8);
+//        OthelloPlayer players[] = {new MiniMaxOthelloPlayer(3), new OthelloRandomPlayer()};
+//
+//        int count = 0;
+//        do{
+//            // Display the current state in the console:
+//            System.out.println("\nCurrent state, " + OthelloState.PLAYER_NAMES[state.nextPlayerToMove] + " to move:");
+//            //state.printBoard();
+//            System.out.println(state);
+//            // Get the move from the player:
+//            OthelloMove move = players[state.nextPlayerToMove].getMove(state);
+//           // System.out.println(move);
+//            state = state.applyMoveCloning(move);
+//
+//        }while(!state.gameOver());
+//
+//        // Show the result of the game:
+//        System.out.println("\nFinal state with score: " + state.score());
+//        //state.printBoard();
+//        System.out.println(state);
+        stats();
     }
 
     public static void stats() {
